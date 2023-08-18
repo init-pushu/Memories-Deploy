6e32ebb3b66db503ee434a444491416506455bf7
Pushkar <pushkaranmol100@gmail.com>
2023-08-18 18:27:25 +0530

    fixed

--- server/index.js
+++ server/index.js
@@ -16,8 +16,6 @@ dotenv.config();
 
 const app = express();
 
-const __filename =  fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname,'../client/build')));
 
 
